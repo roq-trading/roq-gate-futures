a874a6250be0159b357d240f7aca87b5a35148ef
thraneh <thraneh@gmail.com>
2022-08-24 15:42:19 +0200

    fix scripts

--- src/roq/gate_futures/live-usdt.sh
+++ src/roq/gate_futures/live-usdt.sh
@@ -20,9 +20,9 @@ WS_URI="wss://fx-ws.$URI/v4/ws/usdt"
 $PREFIX ./roq-gate-futures \
 	--name "gate-futures" \
 	--config_file "$CONFIG_FILE" \
-  --event_log_dir "$HOME/var/lib/roq/data" \                                                                            
-  --event_log_symlink \                                                                                                 
-  --client_listen_address "$HOME/run/$NAME.sock" \                                                                      
+  --event_log_dir "$HOME/var/lib/roq/data" \
+  --event_log_symlink \
+  --client_listen_address "$HOME/run/$NAME.sock" \
   --metrics_listen_address "$HOME/run/${NAME}_metrics.sock" \
 	--rest_uri "$REST_URI" \
 	--ws_uri "$WS_URI" \
