352f80ae7a61ddb7864d08a258f946f213cdc083
thraneh <thraneh@gmail.com>
2022-10-21 09:41:21 +0200

    refactor rest to better deal with exceptional cases

--- src/roq/gate_futures/rest.hpp
+++ src/roq/gate_futures/rest.hpp
@@ -83,6 +83,9 @@ class Rest final : public web::rest::Client::Handler {
 
   void check_request_queue(std::chrono::nanoseconds now);
 
+  template <typename SuccessHandler, typename ErrorHandler>
+  void process_response(web::rest::Response const &, SuccessHandler, ErrorHandler);
+
  private:
   Handler &handler_;
   // config
