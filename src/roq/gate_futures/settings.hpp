83359daba390c248d88b76b241f2967e72f3e32f
thraneh <thraneh@gmail.com>
2023-07-12 10:11:36 +0200

    update

--- src/roq/gate_futures/settings.hpp
+++ src/roq/gate_futures/settings.hpp
@@ -2,10 +2,11 @@
 
 #pragma once
 
+#include <fmt/chrono.h>
 #include <fmt/compile.h>
 #include <fmt/format.h>
 
-#include "roq/server.hpp"
+#include "roq/server/flags/settings.hpp"
 
 #include "roq/gate_futures/flags/common.hpp"
 #include "roq/gate_futures/flags/rest.hpp"
@@ -14,8 +15,8 @@
 namespace roq {
 namespace gate_futures {
 
-struct Settings final : public server::Settings {
-  explicit Settings(server::Type);
+struct Settings final : public server::flags::Settings {
+  explicit Settings(args::Parser const &, server::Type);
 
   std::string_view exchange;
 
