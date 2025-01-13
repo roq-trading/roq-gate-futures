832c56d2becb485f2b8423080ba889480599724c
thraneh <thraneh@gmail.com>
2025-01-13 04:27:21 +0100

    server/gateway mass-quote interfaces

--- src/roq/gate_futures/gateway.cpp
+++ src/roq/gate_futures/gateway.cpp
@@ -10,6 +10,8 @@
 
 #include "roq/clock.hpp"
 
+#include "roq/server/oms/exceptions.hpp"
+
 #include "roq/gate_futures/json/utils.hpp"
 
 using namespace std::literals;
@@ -168,6 +170,14 @@ uint16_t Gateway::operator()(
 uint16_t Gateway::operator()(Event<CancelAllOrders> const &event, std::string_view const &request_id) {
   assert(!std::empty(event.value.account));
   return get_drop_copy(event.value.account)(event, request_id);
+}
+
+uint16_t Gateway::operator()(Event<MassQuote> const &) {
+  throw server::oms::NotSupported{"not supported"sv};
+}
+
+uint16_t Gateway::operator()(Event<CancelQuotes> const &) {
+  throw server::oms::NotSupported{"not supported"sv};
 }
 
 void Gateway::operator()(metrics::Writer &writer) {
