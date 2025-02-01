de54520e75e3c0bc2a43b613660c5b1c063bdfe1
thraneh <thraneh@gmail.com>
2025-02-01 11:38:15 +0100

    update

---
+++ src/roq/gate_futures/test-usdt.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+if [ "$1" == "debug" ]; then
+  PREFIX="gdb --args"
+else
+  PREFIX=
+fi
+
+NAME="gate-futures"
+
+CONFIG="${CONFIG:-$NAME}"
+
+CONFIG_FILE="$ROQ_CONFIG_PATH/roq-gate-futures/$CONFIG.toml"
+
+URI="testnet.gateio.ws"
+
+REST_URI="https://fx-api-$URI/api/v4"
+WS_URI="wss://fx-ws-$URI/v4/ws/usdt"
+
+$PREFIX ./roq-gate-futures \
+  --name "gate-futures" \
+  --config_file "$CONFIG_FILE" \
+  --cache_dir "$HOME/var/lib/roq/cache" \
+  --event_log_dir "$HOME/var/lib/roq/data" \
+  --event_log_symlink true \
+  --client_listen_address "$HOME/run/$NAME.sock" \
+  --service_listen_address "$HOME/run/metrics/${NAME}.sock" \
+  --rest_uri "$REST_URI" \
+  --ws_uri "$WS_URI" \
+  --api "usdt" \
+  $@
