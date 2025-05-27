dc2d093495b860f6ddb21825fd7a5caa6e4bc40d
thraneh <thraneh@gmail.com>
2025-05-27 09:57:55 +0200

    new gateway dispatcher

--- src/roq/gate_futures/gateway.hpp
+++ src/roq/gate_futures/gateway.hpp
@@ -75,6 +75,9 @@ struct Gateway final : public server::Handler, public Rest::Handler, public Orde
   template <typename... Args>
   void dispatch(Args &&...);
 
+  template <typename... Args>
+  static void dispatch_helper(auto &self, Args &&...);
+
   DropCopy &get_drop_copy(std::string_view const &account);
   OrderEntry &get_order_entry(std::string_view const &account);
 
